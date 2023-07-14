5611644a350574f3e9fbdb8e151efc74e6c976e3
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-07-14 17:36:03 +0200

    Made frontpage buttons

--- apps/WorkTime/WorkTime/MainPage.xaml.cs
+++ apps/WorkTime/WorkTime/MainPage.xaml.cs
@@ -2,23 +2,22 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+
+        private void InClicked(object sender, EventArgs e)
         {
-            count++;
+            SemanticScreenReader.Announce(InBtn.Text);
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void OutBtn_Clicked(object sender, EventArgs e)
+        {
+            SemanticScreenReader.Announce(OutBtn.Text);
         }
     }
 }
