9590dae49930fd783e3b0e504c6f55e4e1015f4e
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-01-23 22:42:48 +0100

    made commandline work

--- src/projects/Marada.Cli/CliEntry.cs
+++ src/projects/Marada.Cli/CliEntry.cs
@@ -1,13 +1,56 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
+using System.CommandLine.Parsing;
+
 using static System.Console;
 
 namespace Marada.Cli;
 
 class CliEntry
 {
+    static string version = "1.0";
+    static string description = "MARADA Cli";
+
     public static async Task<int> Main(string[] args)
     {
-        WriteLine("Test"); 
-        return 0;       
+        var systemInfoCommand = new Command("info", "Displays systeminfo");
+
+        var templateCommand = new Command("template", "Creates a template.");
+        var backendTemplateOption = new Option<string>(
+            name: "--backend",
+            description: "Creates a backend template with the provided argument as its name."
+            );
+        var argumentDbConn = new Argument<string>(
+            name: "db",
+            description: "The connection string for the database to use.");
+
+        templateCommand.AddOption(backendTemplateOption);
+        templateCommand.AddArgument(argumentDbConn);
+        
+        var rootCommand = new RootCommand(description);
+        
+        rootCommand.AddCommand(templateCommand);
+        rootCommand.AddCommand(systemInfoCommand);
+
+        systemInfoCommand.SetHandler(() => ExecuteSystemInfoCommand());
+
+        templateCommand.SetHandler(ExecuteBackendCommand,
+            argumentDbConn,
+            backendTemplateOption
+        );
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    internal static void ExecuteSystemInfoCommand()
+    {
+        WriteLine($"{DateTime.Now}");
+    }
+
+    internal static async Task ExecuteBackendCommand(string backendName, string dbconn)
+    {
+        WriteLine($"mrd: {backendName}, {dbconn}");
     }
 }
