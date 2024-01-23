54eacd17f576f06e342d4d635d092df64d696dc1
Mads Mikkel Rasmussen <mara@aspit.dk>
2024-01-23 16:39:03 +0100

    created CliEntry

---
+++ src/projects/Marada.Cli/CliEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Marada.Cli;
+
+class CliEntry
+{
+    public static async Task<int> Main(string[] args)
+    {
+        WriteLine("Test"); 
+        return 0;       
+    }
+}
